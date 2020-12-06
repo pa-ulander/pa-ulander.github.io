59b67ea364f0aa871f902059e6457587033600d8
pa <pa@kabelkultur.se>
2020-12-07 00:52:16 +0100

    blog stubs

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="sticky-nav">
+      <a href="/" className="sticky-nav-button">
+        Home
+      </a>
+      <div className="sticky-nav-stack">
+        <a href="/" className="sticky-nav-button">
+          Blog
+        </a>
+        <a href="/" className="sticky-nav-button">
+          Contact
+        </a>
+        <a href="/" className="sticky-nav-button">
+          About Us
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
